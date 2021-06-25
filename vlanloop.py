58e039ee432a429dda249806aa2a26b8e3ebcd55
CYBERSEC <82158337+cybersec0@users.noreply.github.com>
2021-06-25 11:27:49 +0530

    Create vlanloop.py

---
+++ vlanloop.py
@@ -0,0 +1,27 @@
+
+import getpass
+import telnetlib
+
+HOST = "10.1.1.1"
+user = input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"P@ssword\n")
+tn.write(b"conf t\n")
+for n in range (10,100):
+        tn.write(b"vlan " + str(n).encode('ascii') + b"\n")
+        tn.write(b"name Python_VLAN_" + str(n).encode('ascii') + b"\n")
+
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
