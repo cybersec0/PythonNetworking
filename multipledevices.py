42f1c9dce1a746dde8db1edfe117b3631e57bd1f
CYBERSEC <82158337+cybersec0@users.noreply.github.com>
2021-06-25 11:29:54 +0530

    Create multipledevices.py

---
+++ multipledevices.py
@@ -0,0 +1,34 @@
+
+import getpass
+import telnetlib
+
+HOST = "localhost"
+user = input("Enter username: ")
+password = getpass.getpass()
+
+f = open ('switches')
+
+for IP in f:
+      IP = IP.strip()
+    print ("Configuring Switch " + (IP))
+    HOST = IP
+    tn = telnetlib.Telnet(HOST)
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n") 
+    if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+
+    tn.write(b"enable\n")
+    tn.write(b"P@ssword\n")
+    tn.write(b"conf t\n")
+    tn.write(b"vlan 2\n")
+    tn.write(b"name Py_vlan2\n")
+    tn.write(b"vlan 3\n")
+    tn.write(b"name Py_vlan3\n")
+    tn.write(b"vlan 4\n")
+    tn.write(b"name Py_vlan4\n")
+    tn.write(b"end\n")
+    tn.write(b"exit\n")
+
+    print(tn.read_all().decode('ascii'))
